89f4ffbf02b3528ab2cbc213e0566120ab7c8156
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-25 06:38:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,6 +59,14 @@ class Dog
   end 
   
   def self.find_or_create_by(name:)
+    song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
   end 
   
   def self.new_from_db(row)
