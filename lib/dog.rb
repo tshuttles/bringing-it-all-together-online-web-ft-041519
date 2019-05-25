2d8f85bcc3b6d22eb6d83a8c08b6b6a45244418a
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-25 06:41:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,10 +70,7 @@ class Dog
   end 
   
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    breed = row[2]
-    self.new(id, name, breed)
+    Dog.new(id: row[0], name: row[1], breed: row[2])
   end 
   
   def self.find_by_name(name) 
