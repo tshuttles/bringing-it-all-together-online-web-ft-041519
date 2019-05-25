2addd8b1d77c0ec1cb2cb4e273af4f1e1ef005fc
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-25 06:09:35 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,37 @@
+class Dog 
+  
+  attr_accessor :name, :breed 
+  
+  def initialize(id=nil, name, breed)
+    @id = id 
+    @name = name 
+    @breed = breed 
+  end 
+  
+  def self.create_table 
+  end 
+  
+  def self.drop_table 
+  end 
+  
+  def save
+  end 
+  
+  def self.create 
+  end 
+  
+  def self.find_by_id 
+  end 
+  
+  def self.find_or_create_by 
+  end 
+  
+  def self.new_from_db 
+  end 
+  
+  def self.find_by_name 
+  end 
+  
+  def update 
+  end 
+end 
